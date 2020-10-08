4ca1e0bd4381d73695c837fcbd8e1a8640f18cdc
arjun17697 <arjun17697@gmail.com>
2020-10-08 09:45:39 +0530

    Fix:Fixed basic problems of assigning and logic

--- src/com/bridgelabz/main/TicTacToeGame.java
+++ src/com/bridgelabz/main/TicTacToeGame.java
@@ -1,9 +1,17 @@
 package com.bridgelabz.main;
 
 public class TicTacToeGame {
-	static void UC1_charAssign() {
-		char[] boardArray= new char[10];
+	char[] boardElement = new char[10];
+
+	public char[] UC1_createBoard() {
+		for (int i = 1; i < 10; i++) {
+			boardElement[i] = ' ';
+		}
+		return boardElement;
 	}
 
+	public static void main(String[] args) {
+		TicTacToeGame ticTacToe = new TicTacToeGame();
+		char[] boardElement = ticTacToe.UC1_createBoard();
+	}
 }
-
