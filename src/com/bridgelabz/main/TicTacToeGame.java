b7230b617bc7cb210feaeb7bcfe2d0c4b8049bbc
arjun17697 <arjun17697@gmail.com>
2020-10-08 11:30:05 +0530

    Add:Method to show board

--- src/com/bridgelabz/main/TicTacToeGame.java
+++ src/com/bridgelabz/main/TicTacToeGame.java
@@ -3,9 +3,10 @@ package com.bridgelabz.main;
 import java.util.Scanner;
 
 public class TicTacToeGame {
-	char[] boardElement = new char[10];
+	static char[] boardElement = new char[10];
 	static char compKey;
 	static char playerKey;
+
 //Assigning Board Elements
 	public char[] UC1_createBoard() {
 		for (int i = 1; i < 10; i++) {
@@ -13,38 +14,52 @@ public class TicTacToeGame {
 		}
 		return boardElement;
 	}
-	
+
 //Method to choose between X and O
 	public static void UC2_chooseKey() {
-		Scanner sc=new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Press 1 for choosing X as your key or 2 for choosing O as your key");
 		int choice = sc.nextInt();
-		switch(choice) {
+		switch (choice) {
 		case 1:
-			compKey='O';
-			playerKey= 'X';
-			System.out.println("Player Key:"+playerKey);
-			System.out.println("Computer Key" +compKey);
+			compKey = 'O';
+			playerKey = 'X';
+			System.out.println("Player Key:" + playerKey);
+			System.out.println("Computer Key" + compKey);
 			break;
 		case 2:
-			compKey='X';
-			playerKey= 'O';
-			System.out.println("Player Key:"+playerKey);
-			System.out.println("Computer Key" +compKey);
+			compKey = 'X';
+			playerKey = 'O';
+			System.out.println("Player Key:" + playerKey);
+			System.out.println("Computer Key" + compKey);
 			break;
-		default: 
+		default:
 			System.out.println("Invalid Input.Please enter again");
 			UC2_chooseKey();
-			break;	
+			break;
 		}
-		
+
+
 	}
+	
+	
+	/**
+	 * Method to show the TicTacToe Board
+	 */
+	public static void showBoard_UC3() {
+		System.out.println(boardElement[1]+"|"+boardElement[2]+"|"+boardElement[3]);
+		System.out.println("~~~~~~~~~~~~~~");
+		System.out.println(boardElement[4]+"|"+boardElement[5]+"|"+boardElement[6]);
+		System.out.println("~~~~~~~~~~~~~~");
+		System.out.println(boardElement[7]+"|"+boardElement[8]+"|"+boardElement[9]);
+		}
 
 	public static void main(String[] args) {
 		TicTacToeGame ticTacToe = new TicTacToeGame();
 		char[] boardElement = ticTacToe.UC1_createBoard();
-		
+
 		UC2_chooseKey();
-		
+		showBoard_UC3();
+
 	}
 }
