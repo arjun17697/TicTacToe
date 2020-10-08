6db094ea2d9028b05f2d66c9a9aad2203a4cd91b
arjun17697 <arjun17697@gmail.com>
2020-10-08 20:00:23 +0530

    Add:Computer Moves

--- src/com/bridgelabz/main/TicTacToeGame.java
+++ src/com/bridgelabz/main/TicTacToeGame.java
@@ -11,6 +11,7 @@ public class TicTacToeGame {
 	static int rand_int1;
 	static char turn;
 	static boolean flag=false;
+	static int totalMoves=0;
 
 	public static int getRand_int1() {
 		return rand_int1;
@@ -64,6 +65,13 @@ public class TicTacToeGame {
 	/**
 	 * Method to move to the desired position
 	 */
+	public static void makeMove() {
+	if(rand_int1==1) {
+		moveOnBoardandAssignKeyUC4_5(playerKey);
+	}else {
+		playComputerMove();
+				}
+	}
 	
 	public static void moveOnBoardandAssignKeyUC4_5(char turn) {
 		Scanner sc = new Scanner(System.in);
@@ -72,21 +80,19 @@ public class TicTacToeGame {
 		position = sc.nextInt();
 		if (position > 9 || position < 1 && boardElement[position] != ' ') {
 			System.out.println("You have entered invalid position.Please enter again");
-			moveOnBoardandAssignKeyUC4_5(turn);
+			moveOnBoardandAssignKeyUC4_5(playerKey);
 		
 
 		} else 
 			{
-			boardElement[position] = turn;
+			boardElement[position] = playerKey;
 			showBoard_UC3();
 			winningCondition_UC7();
+			totalMoves++;
+			turn=compKey;
+			playComputerMove();
+				
 			
-				if (turn==playerKey) {
-				turn=compKey;
-				}else if (turn==compKey) {
-					turn=playerKey;
-				}
-			moveOnBoardandAssignKeyUC4_5(turn);
 	}	
 	}
 	
@@ -130,6 +136,7 @@ public class TicTacToeGame {
 			flag= true;
 			return flag;
 			
+			
 		}
 
 		else if (((boardElement[1] == compKey) && (boardElement[2] == compKey) && (boardElement[3] == compKey))
@@ -150,6 +157,95 @@ public class TicTacToeGame {
 		}
 
 	}
+	
+	public static void playComputerMove() {
+		
+		if (totalMoves < 2) {
+			placeRandomly();
+			
+		} else {
+			// rows:
+			// row1
+			if (boardElement[1] == compKey && boardElement[2] == compKey && boardElement[3] == ' ')
+				boardElement[3] = compKey;
+			else if (boardElement[1] == compKey && boardElement[3] == compKey && boardElement[2] == ' ')
+				boardElement[2] = compKey;
+			else if (boardElement[2] == compKey && boardElement[3] == compKey && boardElement[1] == ' ')
+				boardElement[1] = compKey;
+			// row2
+			else if (boardElement[4] == compKey && boardElement[5] == compKey && boardElement[6] == ' ')
+				boardElement[6] = compKey;
+			else if (boardElement[5] == compKey && boardElement[6] == compKey && boardElement[4] == ' ')
+				boardElement[4] = compKey;
+			else if (boardElement[4] == compKey && boardElement[6] == compKey && boardElement[5] == ' ')
+				boardElement[5] = compKey;
+			// row3
+			else if (boardElement[7] == compKey && boardElement[8] == compKey && boardElement[9] == ' ')
+				boardElement[9] = compKey;
+			else if (boardElement[8] == compKey && boardElement[9] == compKey && boardElement[7] == ' ')
+				boardElement[7] = compKey;
+			else if (boardElement[7] == compKey && boardElement[9] == compKey && boardElement[8] == ' ')
+				boardElement[8] = compKey;
+			// columns:
+			// column1:
+			else if (boardElement[1] == compKey && boardElement[4] == compKey && boardElement[7] == ' ')
+				boardElement[7] = compKey;
+			else if (boardElement[1] == compKey && boardElement[7] == compKey && boardElement[4] == ' ')
+				boardElement[4] = compKey;
+			else if (boardElement[4] == compKey && boardElement[7] == compKey && boardElement[1] == ' ')
+				boardElement[1] = compKey;
+			// column2
+			else if (boardElement[2] == compKey && boardElement[5] == compKey && boardElement[8] == ' ')
+				boardElement[8] = compKey;
+			else if (boardElement[2] == compKey && boardElement[8] == compKey && boardElement[5] == ' ')
+				boardElement[5] = compKey;
+			else if (boardElement[5] == compKey && boardElement[8] == compKey && boardElement[2] == ' ')
+				boardElement[2] = compKey;
+			// column3
+			else if (boardElement[3] == compKey && boardElement[6] == compKey && boardElement[9] == ' ')
+				boardElement[9] = compKey;
+			else if (boardElement[3] == compKey && boardElement[9] == compKey && boardElement[6] == ' ')
+				boardElement[6] = compKey;
+			else if (boardElement[6] == compKey && boardElement[9] == compKey && boardElement[3] == ' ')
+				boardElement[3] = compKey;
+			// diagonals
+			// diagonal1
+			else if (boardElement[1] == compKey && boardElement[5] == compKey && boardElement[9] == ' ')
+				boardElement[9] = compKey;
+			else if (boardElement[1] == compKey && boardElement[9] == compKey && boardElement[5] == ' ')
+				boardElement[5] = compKey;
+			else if (boardElement[5] == compKey && boardElement[9] == compKey && boardElement[1] == ' ')
+				boardElement[1] = compKey;
+			// diagonal2
+			else if (boardElement[3] == compKey && boardElement[5] == compKey && boardElement[7] == ' ')
+				boardElement[7] = compKey;
+			else if (boardElement[3] == compKey && boardElement[7] == compKey && boardElement[5] == ' ')
+				boardElement[5] = compKey;
+			else if (boardElement[5] == compKey && boardElement[7] == compKey && boardElement[3] == ' ')
+				boardElement[3] = compKey;
+				
+				
+				
+			else
+				placeRandomly();
+				}
+		winningCondition_UC7();
+		
+	}
+	
+	public static void placeRandomly() {
+		int computerTurn = 0;
+		while (true) {
+			computerTurn = 1 + (int) (Math.random() * 10) % 9;
+			if (boardElement[computerTurn] == ' ') {
+				boardElement[computerTurn] = compKey;
+				showBoard_UC3();
+				moveOnBoardandAssignKeyUC4_5(playerKey);
+				
+				break;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
 		TicTacToeGame ticTacToe = new TicTacToeGame();
@@ -157,16 +253,10 @@ public class TicTacToeGame {
 		
 		UC2_chooseKey();
 		toss_UC6();
+		 makeMove();
 		showBoard_UC3();
 		moveOnBoardandAssignKeyUC4_5(turn);
-		/*winningCondition_UC7();
-		showBoard_UC3();
-		moveOnBoardandAssignKeyUC4_5(compKey);
-		winningCondition_UC7();
-		showBoard_UC3();
-		moveOnBoardandAssignKeyUC4_5(playerKey);
-		showBoard_UC3();
-		winningCondition_UC7();*/
+	
 
 	}
 }
